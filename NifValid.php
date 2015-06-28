4e0afe325576d7551fa529995df339688c851f72
Luis <a70130@alunos.uminho.pt>
2015-06-28 18:13:23 +0100

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	functions.php
    	nbproject/private/private.xml
    	validNif.php

--- NifValid.php
+++ NifValid.php
@@ -18,3 +18,61 @@ class NifValid {
         
     }
 }
+
+class Name{
+    public $name = "Luis";
+    public $number = 5;
+    public $teste = false;
+    public $teste2 = true;
+    public $value5 = 30;
+    public $value2 = 32;
+    public $contagem = 0;
+    public $niflenght = 9;
+    public $verdadeiro = true;
+    public $falso = false;
+    public $morada = null;
+    public $encomenda = null;
+    public $variavel = true;
+    public $variavel2 = null;
+    public $variavel3 = 10;
+    public $variavel4 = 11;
+            
+    
+    public function verifica(){
+          
+        if($name = "Luis"){
+            return true;
+        }
+        if($number = 6){
+            return true;
+        }
+        if($teste = false){
+            return true;
+        }
+        if($value5 = 20){
+            return false;
+        }
+        if($value2 = 32){
+            return $value2;
+        }
+        if($contagem = 0){
+            $contagem++;
+        }
+        if($niflenght = 9){
+            return "Nif válido";
+        }
+        if($verdadeiro = true){
+            return $verdadeiro;
+        }
+        if($falso = true){
+            return "Erro";
+        }
+        if($encomenda = "feito"){
+            return "Encomenda criada";
+        }
+        if($encomenda = null){
+            return "Encomenda por criar";
+        }
+    }
+        
+}
